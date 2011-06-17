1acf04e6baa47694781e6b7fb724f71f2a0cc978
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2011-06-17 21:14:22 +0200

    Added nop Brain.

--- src/Brain.hs
+++ src/Brain.hs
@@ -1,7 +1,19 @@
-module Brain (Brain) where
+module Brain (
+
+  -- * The Brain type
+  Brain,
+
+  -- * Trivial brains
+  nop
+
+  ) where
 
 import Core
 import Logic
 
 -- | Suggest a move for the current proponent.
 type Brain m = Board -> m Move
+
+-- | The brain that always suggests @Move CardToField 0 I@.
+nop :: Monad m => Brain m
+nop _ = return (Move CardToField 0 I)
