47016e3eff04390a8cd391aaf34647614f0168f0
Tom Lokhorst <tom@lokhorst.eu>
2011-06-17 07:48:32 +0200

    Started with Slots

--- src/Slots.hs
+++ src/Slots.hs
@@ -1,4 +1,28 @@
 module Slots where
 
+import Cards
 
+data Slot = Slot Field Vitality
+  deriving Show
+
+type Vitality = Int
+
+data Field
+  = Value Int
+  | Function Card
+  deriving Show
+
+
+slotNr :: Slot -> Maybe Int
+slotNr (Slot (Value x) _)
+  | 0 <= x && x <= 255    = Just x
+slotNr _                  = Nothing
+
+dead :: Slot -> Bool
+dead (Slot _ x)
+  | x == -1 || x == 0 = True
+dead _                = False
+
+alive :: Slot -> Bool
+alive = not . dead
 
