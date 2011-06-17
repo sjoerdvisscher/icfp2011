6470242d55f76bc72ae50310c900fedf021c87db
Tom Lokhorst <tom@lokhorst.eu>
2011-06-18 00:15:59 +0200

    Clean up ltg.hs

--- src/ltg.hs
+++ src/ltg.hs
@@ -9,23 +9,30 @@ import Control.Monad.Error
 import Control.Monad.Identity
 import Control.Monad.State
 
-main :: IO ()
-main = do
-  putStrLn "Lambda: The Gathering, by Magic Missiles"
-  go emptyBoard 0
+play :: Brain -> Brain -> IO ()
+play b1 b2 = do
+  putStrLn $ "Player #0"
+  (openingMove, brain1) <- playFirst b1
+  brain2 <- playSecond b2
+  go openingMove emptyBoard 1 brain2 brain1
   where
-    go :: Board -> Int -> IO ()
-    go board i = do
-      putStrLn $ "Player #" ++ show i
-      move <- fst <$> playFirst stdinBrain
+    go :: Move -> Board -> Int -> NextBrain -> NextBrain -> IO ()
+    go move board i curr next = do
       print move
-      case runIdentity $ runErrorT $ runStateT (step move) board of
-        Right (rp, s') -> putStrLn rp >> go s' (1 - i)
-        Left err       -> putStrLn err
+      let Right (rp, board') = runIdentity $ runErrorT $ runStateT (step move) board
+      putStrLn $ "Player #" ++ show i
+      (move', brain') <- nextMove curr move board'
+      putStr rp
+      go move' board' (1 - i) next brain'
     step move = do
       mb <- turn move
       s <- report <$> get
       return $ maybe "" (++"\n") mb ++ s
+
+main :: IO ()
+main = do
+  putStrLn "Lambda: The Gathering, by Magic Missiles"
+  play stdinBrain nopBrain
 
 report :: Board -> String
 report board = concatMap pr (zip slots [0 :: Int ..])
