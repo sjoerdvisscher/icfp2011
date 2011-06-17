600478277a4a9ef9924d91749dd83644f9f9fcfc
Tom Lokhorst <tom@lokhorst.eu>
2011-06-17 07:46:16 +0200

    Finished all cards, defined Show and Read instances

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,7 +1,56 @@
 module Cards where
 
 data Card
- = I
- | Zero
- deriving Show
+  = I
+  | Zero
+  | Succ
+  | Dbl
+  | Get
+  | Put
+  | S
+  | K
+  | Inc
+  | Dec
+  | Attack
+  | Help
+  | Copy
+  | Revive
+  | Zombie
+
+instance Show Card where
+  show I      = "I"
+  show Zero   = "zero"
+  show Succ   = "succ"
+  show Dbl    = "dbl"
+  show Get    = "get"
+  show Put    = "put"
+  show S      = "S"
+  show K      = "K"
+  show Inc    = "inc"
+  show Dec    = "dec"
+  show Attack = "attack"
+  show Help   = "help"
+  show Copy   = "copy"
+  show Revive = "revive"
+  show Zombie = "zombie"
+
+instance Read Card where
+  readsPrec _ = f
+    where
+      f "I"      = [(I, "")]
+      f "zero"   = [(Zero, "")]
+      f "succ"   = [(Succ, "")]
+      f "dbl"    = [(Dbl, "")]
+      f "get"    = [(Get, "")]
+      f "put"    = [(Put, "")]
+      f "S"      = [(S, "")]
+      f "K"      = [(K, "")]
+      f "inc"    = [(Inc, "")]
+      f "dec"    = [(Dec, "")]
+      f "attack" = [(Attack, "")]
+      f "help"   = [(Help, "")]
+      f "copy"   = [(Copy, "")]
+      f "revive" = [(Revive, "")]
+      f "zombie" = [(Zombie, "")]
+      f _        = []
 
