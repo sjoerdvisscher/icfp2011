e45d5cfca3cf86794a9aa16f573f8bb34f5f1203
Sjoerd Visscher <sjoerd@w3future.com>
2011-06-18 18:23:46 +0200

    new names

--- src/Brain/Sjoerd.hs
+++ src/Brain/Sjoerd.hs
@@ -9,8 +9,8 @@ sjoerdBrain = brainFromMoves . ($ repeat nop)
   $ attackAll 255 255
 
 attackAll :: Int -> Int -> [Move] -> [Move]
-attackAll 1 _ = id
-attackAll me him = 
-    attack me him 5556 123
-  . attack (me - 1) him 5556 123
-  . attackAll (me - 2) (him - 1)
+attackAll (-1) _ = id
+attackAll us it = 
+    attack us it 5556 123
+  . attack (us - 1) it 5556 123
+  . attackAll (us - 2) (it - 1)
