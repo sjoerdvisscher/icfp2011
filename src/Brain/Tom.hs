6ffbd9b71a83e79ca0ad7e504cc2cb4e9b4deec9
Tom Lokhorst <tom@lokhorst.eu>
2011-06-19 16:26:29 +0200

    Some bugfixes in tomBrain

--- src/Brain/Tom.hs
+++ src/Brain/Tom.hs
@@ -22,9 +22,8 @@ tomBrain = toBrain $ do
 
   -- Attack first
   -- random
-  let attackSlot = 124
-  attack attackSlot 0 5556 123
-  attack attackSlot 0 5556 123
+  attack 128 0 5556 123
+  attack 129 0 5556 123
 
   -- break
   forever $ do
@@ -33,38 +32,48 @@ tomBrain = toBrain $ do
     zombieSlot `applyFieldToCard` Help
     (opSlot1:_) <- interesting
     zombieSlot `applyInt` opSlot1
-    (opSlot2:_) <- (\\ [opSlot1]) <$> interesting
+    (opSlot2:_) <- exclude [opSlot1] <$> interesting
     zombieSlot `applyInt` opSlot2
     K `applyCardToField` zombieSlot
     S `applyCardToField` zombieSlot
 
-    (zombieArgSlot:_) <- (\\ [zombieSlot]) <$> free
+    --printState
+    --break
+    (zombieArgSlot:_) <- exclude [zombieSlot] <$> free
     v <- MonadBrain.vitality opSlot1 opponent
     load v zombieArgSlot
     K `applyCardToField` zombieArgSlot
     zombieSlot `apply` zombieArgSlot
+    Put `applyCardToField` zombieArgSlot
 
     -- alert "before zombie"
     -- break
 
     -- todo: get their dead slots
-    (zombieAttackSlot:_) <- (\\ [zombieSlot, zombieArgSlot]) <$> free
+    frees <- free
+    --printState
+    --alert $ "free: " ++ show frees
+    -- break
+    (zombieAttackSlot:_) <- exclude [zombieSlot, zombieArgSlot] <$> free
     zombieAttackSlot `applyFieldToCard` Zombie
     (opSlot3:_) <- deadSlots
-    zombieAttackSlot `applyInt` opSlot3
+    zombieAttackSlot `applyInt` (255 - opSlot3)
     zombieAttackSlot `apply` zombieSlot
+    Put `applyCardToField` zombieSlot
 
     -- alert "after zombie"
     -- break
+
+exclude :: [SlotNr] -> [SlotNr] -> [SlotNr]
+exclude blackList sluts = sluts \\ blackList
 
 -- | Opponent's slots that are alive, sorted by size
 interesting :: B [SlotNr]
 interesting = do
   slts <- slots opponent
-   -- shuffle
   let ss = map snd . reverse . sortBy (comparing fst) . reverse . (map . first $ size . Core.field)
              $ filter (alive . fst) $ zip (V.toList slts) [0 :: Int ..]
-  return $ ss ++ [0..] -- In case there's a bug, return everything
+  return $ ss ++ [0..255] -- In case there's a bug, return everything
 
 -- | Dead's slots that are dead, with the biggest expresssion
 deadSlots :: B [SlotNr]
